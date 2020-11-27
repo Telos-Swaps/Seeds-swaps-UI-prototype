0a74132ffa7eeb09a6b6085612b95527bf16b0f5
Rory Mapstone <rory.mapstone@gmail.com>
2020-11-27 12:42:08 +0200

    Drop unwanted modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,6 @@ import Vuex from "vuex";
 import { GeneralModule } from "./modules/general";
 import { EosTransitModule } from "./modules/wallet/tlosWallet";
 import { TlosBancorModule } from "./modules/swap/tlosBancor";
-import { UsdBancorModule } from "./modules/swap/usdSx";
-import { xChainModule } from "./modules/swap/xChain";
 import { BancorModule } from "./modules/swap/index";
 import { WalletModule } from "./modules/wallet/index";
 import { NetworkModule } from "./modules/network/index";
@@ -16,8 +14,6 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
-    ...extractVuexModule(UsdBancorModule),
-    ...extractVuexModule(xChainModule),
     ...extractVuexModule(TlosBancorModule),
     ...extractVuexModule(GeneralModule),
     ...extractVuexModule(EosTransitModule),
@@ -34,8 +30,6 @@ export const vxm = {
   wallet: createProxy(store, WalletModule),
   tlosWallet: createProxy(store, EosTransitModule),
   tlosBancor: createProxy(store, TlosBancorModule),
-  usdsBancor: createProxy(store, UsdBancorModule),
-  xchainBancor: createProxy(store, xChainModule),
   bancor: createProxy(store, BancorModule),
   tlosNetwork: createProxy(store, TlosNetworkModule),
   network: createProxy(store, NetworkModule)
